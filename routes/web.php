f8faaa70896e52818878190ce4092d9518c7e17e
Yehor Smoliakov <yehors+gh@ukr.net>
2020-09-01 18:20:07 +0300

    Add names

--- routes/web.php
+++ routes/web.php
@@ -7,15 +7,15 @@ use App\Http\Controllers\ErrorsController;
 use App\Http\Controllers\TestController;
 
 Route::middleware(SuperBasicAuth::class)->group(function () {
-    Route::get('/logs', [LogViewerController::class, 'index']);
+    Route::get('/logs', [LogViewerController::class, 'index'])->name('logs');
 });
 
 Route::prefix('/auth')->middleware(['api'])->group(function () {
     Route::post('login', 'AuthController@login')->name('login');
-    Route::post('logout', 'AuthController@logout');
-    Route::post('refresh', 'AuthController@refresh');
+    Route::post('logout', 'AuthController@logout')->name('logout');
+    Route::post('refresh', 'AuthController@refresh')->name('refresh');
 });
 
-Route::get('/login', [ErrorsController::class, 'loginRequired'])->name('login');
+Route::get('/login', [ErrorsController::class, 'loginRequired'])->name('login.old');
 
-Route::get('/test/test1', [TestController::class, 'test1']);
+Route::get('/test/test1', [TestController::class, 'test1'])->name('test1');
